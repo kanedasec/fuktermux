513e5c9d1184faff6e3a75fa990ab3eaad4af7d5
kanedasec <vernon.acs@gmail.com>
2023-01-09 16:28:49 -0300

    testing

--- hacking/serverkey.sh
+++ hacking/serverkey.sh
@@ -2,7 +2,8 @@
 
 
 echo "extra-keys = [ \
-[{macro:'nethunter SPACE python3 SPACE -m SPACE http.server SPACE & ENTER', display:'simple http'}], \
+[{macro:'nethunter SPACE python3 SPACE -m SPACE http.server SPACE & ENTER', display:'simple http'}, \
+{macro:'nethunter SPACE python3 SPACE -m SPACE uploadserver SPACE & ENTER', display:'upload http'}], \
 [{macro:'~/fuktermux/hackingkey.sh SPACE ENTER', display:'hacking'}, \
 {macro:'~/fuktermux/regularkey.sh SPACE ENTER', display:'keys'}, \
 {macro:'~/fuktermux/centralkey.sh SPACE ENTER', display:'central'}] \
