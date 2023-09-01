71ceee7728a7fa11548cacdb3903a0b1d9ab2100
kanedasec <vernon.acs@gmail.com>
2023-09-01 19:59:17 -0300

    web_server created

---
+++ resources/web_server.py
@@ -0,0 +1,35 @@
+from flask import Flask, request, render_template
+import subprocess
+import os
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        command = request.form.get('command')
+        if command == 'list_txt_files':
+            try:
+                txt_files = [f for f in os.listdir() if f.endswith('.txt')]
+                return render_template('index.html', txt_files=txt_files)
+            except Exception as e:
+                return str(e)
+        elif command.startswith('cat_txt_file'):
+            try:
+                file_name = command.split(':')[-1]
+                with open(file_name, 'r') as file:
+                    file_contents = file.read()
+                return render_template('index.html', file_contents=file_contents, file_name=file_name)
+            except Exception as e:
+                return str(e)
+        elif command.startswith('delete_txt_file'):
+            try:
+                file_name = command.split(':')[-1]
+                os.remove(file_name)
+                return 'File deleted successfully.'
+            except Exception as e:
+                return str(e)
+    return render_template('index.html', txt_files=None, file_contents=None, file_name=None)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
