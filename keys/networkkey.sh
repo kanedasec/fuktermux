60b2ddf267c774247c36ddfd998aba112a602e0b
kanedasec <vernon.acs@gmail.com>
2023-09-03 16:27:19 -0300

    reworking

--- keys/networkkey.sh
+++ keys/networkkey.sh
@@ -3,7 +3,7 @@
 
 echo "extra-keys = [ \
 [{macro:'nethunter SPACE /home/kali/resources/scripts/livehosts.sh SPACE ENTER', display:'live hosts'}, \
-{macro:'nethunter SPACE /home/kali/resources/scripts/fullscan.sh SPACE ENTER', display:'Full Scan'}, \
+{macro:'nethunter SPACE -r SPACE /home/kali/resources/scripts/fullscan.sh SPACE ENTER', display:'Full Scan'}, \
 {macro:'nethunter SPACE /home/kali/resources/scripts/target.sh', display:'target'}], \
 [{macro:'~/fuktermux/keys/hackingkey.sh SPACE ENTER', display:'hacking'}, \
 {macro:'~/fuktermux/keys/regularkey.sh SPACE ENTER', display:'keys'}, \
