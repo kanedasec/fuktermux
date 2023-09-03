65a27173a3ed07d767f7cd90f82a4df1103c3783
kanedasec <vernon.acs@gmail.com>
2023-09-03 16:14:25 -0300

    reworking

--- keys/serverkey.sh
+++ keys/serverkey.sh
@@ -4,10 +4,10 @@
 echo "extra-keys = [ \
 [{macro:'nethunter SPACE python3 SPACE -m SPACE http.server SPACE & ENTER', display:'simple http'}, \
 {macro:'nethunter SPACE python3 SPACE -m SPACE uploadserver SPACE & ENTER', display:'upload http'}, \
-{macro:'python SPACE SPACE ~/fuktermux/resources/web_server.py SPACE & ENTER', display:'web server'}], \
-[{macro:'~/fuktermux/main/hackingkey.sh SPACE ENTER', display:'hacking'}, \
-{macro:'~/fuktermux/main/regularkey.sh SPACE ENTER', display:'keys'}, \
-{macro:'~/fuktermux/main/centralkey.sh SPACE ENTER', display:'central'}] \
+{macro:'nethunter SPACE python3 SPACE SPACE ~/resources/web_server.py SPACE & ENTER', display:'web server'}], \
+[{macro:'~/fuktermux/keys/hackingkey.sh SPACE ENTER', display:'hacking'}, \
+{macro:'~/fuktermux/keys/regularkey.sh SPACE ENTER', display:'keys'}, \
+{macro:'~/fuktermux/keys/centralkey.sh SPACE ENTER', display:'central'}] \
 ]"  > ~/.termux/termux.properties 
 
 termux-reload-settings
